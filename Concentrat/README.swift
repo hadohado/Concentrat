16673e65a3f8361442cdf5270b60f48810c58821
hd <ha_do@yahoo.com>
2019-11-25 12:08:41 -0800

    each card has own methods     =  touchCard() and touchCard2()

---
+++ Concentrat/README.swift
@@ -0,0 +1,13 @@
+//
+//  README.swift
+//  Concentrat
+//
+//  Created by ha tuong do on 11/25/19.
+//  Copyright © 2019 alpine. All rights reserved.
+//
+
+import Foundation
+// nov-25 will make all cards have same method
+
+// nov-19-2019 each card has own method touchCard() and touchCard2()
+
